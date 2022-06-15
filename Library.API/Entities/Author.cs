e1b3065fc4e6de0ab340c7ee75869e7a13b551e5
Flavia Bajan <bajanf@gmail.com>
2022-06-15 16:01:02 +0300

    remove warnings for the uncommented code

--- Library.API/Entities/Author.cs
+++ Library.API/Entities/Author.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Library.API.Entities
 {
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
     [Table("Authors")]
     public class Author
@@ -22,4 +23,5 @@ namespace Library.API.Entities
 
         public ICollection<Book> Books { get; set; } = new List<Book>();
     }
+#pragma warning disable CS1591
 }
